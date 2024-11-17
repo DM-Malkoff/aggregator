1c8c8fd0c5c90e01058a6aba3c1349e1d6a4547b
DMALKOV\dmalkov <demoncfh@mail.ru>
2024-11-17 23:11:01 +0400

    first commit

---
+++ backend/src/moks/index.ts
@@ -0,0 +1,227 @@
+export const users = [
+  {
+    "_id": "673a22a26b79c9891151f6c9",
+    "index": 0,
+    "guid": "57d756bc-2614-425e-abb5-8bc6ee93b5bc",
+    "isActive": true,
+    "balance": "$3,109.03",
+    "picture": "http://placehold.it/32x32",
+    "age": 32,
+    "eyeColor": "green",
+    "name": "Herminia Hester",
+    "gender": "female",
+    "company": "CONJURICA",
+    "email": "herminiahester@conjurica.com",
+    "phone": "+1 (925) 521-2946",
+    "address": "386 Ingraham Street, Brewster, Palau, 4482",
+    "about": "Ad et consequat ipsum ea anim exercitation do. Eu laborum duis anim velit laborum aute ipsum magna labore mollit. Excepteur qui nostrud excepteur in deserunt aliquip irure eu esse fugiat proident duis sunt ex.\r\n",
+    "registered": "2021-01-07T04:56:12 -04:00",
+    "latitude": -69.818807,
+    "longitude": 86.926018,
+    "tags": [
+      "aliquip",
+      "exercitation",
+      "eiusmod",
+      "velit",
+      "ullamco",
+      "dolor",
+      "in"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Zamora Haney"
+      },
+      {
+        "id": 1,
+        "name": "Martina Sutton"
+      },
+      {
+        "id": 2,
+        "name": "Kemp Randall"
+      }
+    ],
+    "greeting": "Hello, Herminia Hester! You have 2 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "673a22a286b4affafe39dcfc",
+    "index": 1,
+    "guid": "e31757c0-c931-4bb2-9d58-6a02e51d164a",
+    "isActive": true,
+    "balance": "$2,062.89",
+    "picture": "http://placehold.it/32x32",
+    "age": 22,
+    "eyeColor": "green",
+    "name": "Emerson Mullins",
+    "gender": "male",
+    "company": "HAWKSTER",
+    "email": "emersonmullins@hawkster.com",
+    "phone": "+1 (812) 590-3134",
+    "address": "384 Buffalo Avenue, Manitou, Pennsylvania, 6695",
+    "about": "Sunt aute adipisicing ut laborum mollit in cupidatat magna proident. Sint eu consequat qui tempor aute. Irure voluptate incididunt mollit fugiat qui consequat non elit. Eiusmod eiusmod reprehenderit nisi pariatur pariatur laborum mollit in aliquip consectetur. Tempor deserunt tempor qui aliqua laborum nulla pariatur officia. Voluptate nisi voluptate labore sint nulla consequat tempor elit nostrud ut duis id dolor aliquip.\r\n",
+    "registered": "2024-01-01T05:04:23 -04:00",
+    "latitude": -21.212679,
+    "longitude": -75.567324,
+    "tags": [
+      "labore",
+      "adipisicing",
+      "et",
+      "enim",
+      "quis",
+      "ex",
+      "non"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Pace Butler"
+      },
+      {
+        "id": 1,
+        "name": "Yates Snyder"
+      },
+      {
+        "id": 2,
+        "name": "Mason George"
+      }
+    ],
+    "greeting": "Hello, Emerson Mullins! You have 2 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "673a22a25a51b1f8f27a26d1",
+    "index": 2,
+    "guid": "f469f51e-99fe-44ef-b149-475866a0845a",
+    "isActive": false,
+    "balance": "$3,883.18",
+    "picture": "http://placehold.it/32x32",
+    "age": 26,
+    "eyeColor": "green",
+    "name": "Katheryn Beard",
+    "gender": "female",
+    "company": "RECRITUBE",
+    "email": "katherynbeard@recritube.com",
+    "phone": "+1 (871) 404-3546",
+    "address": "555 Boerum Place, Sanborn, Idaho, 8623",
+    "about": "Qui velit fugiat qui aliqua mollit. Non pariatur est nisi labore dolore in minim ipsum adipisicing nulla fugiat nostrud ex. Voluptate ea commodo cupidatat dolor tempor voluptate. Commodo et ullamco elit aliquip irure velit tempor. Deserunt laboris in qui id ullamco mollit ad ullamco exercitation labore nisi culpa. Consectetur labore irure excepteur veniam magna amet id dolore reprehenderit deserunt officia. Mollit eu quis incididunt voluptate tempor esse incididunt sint consectetur.\r\n",
+    "registered": "2016-12-23T11:27:23 -04:00",
+    "latitude": -18.790698,
+    "longitude": -9.107651,
+    "tags": [
+      "magna",
+      "est",
+      "velit",
+      "mollit",
+      "id",
+      "enim",
+      "magna"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Roslyn Watkins"
+      },
+      {
+        "id": 1,
+        "name": "Huffman Frye"
+      },
+      {
+        "id": 2,
+        "name": "Williams Mayer"
+      }
+    ],
+    "greeting": "Hello, Katheryn Beard! You have 9 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "673a22a264d05f904bbccdd4",
+    "index": 3,
+    "guid": "006dd52e-2b85-460e-9b72-69d4068f39be",
+    "isActive": false,
+    "balance": "$2,180.63",
+    "picture": "http://placehold.it/32x32",
+    "age": 20,
+    "eyeColor": "green",
+    "name": "Jeannette Best",
+    "gender": "female",
+    "company": "OLYMPIX",
+    "email": "jeannettebest@olympix.com",
+    "phone": "+1 (826) 525-2136",
+    "address": "246 Lenox Road, Robinette, Utah, 8426",
+    "about": "Fugiat eiusmod cupidatat qui sunt ex do laborum et dolore. Reprehenderit id culpa et enim veniam pariatur cillum. Exercitation reprehenderit consectetur excepteur incididunt labore.\r\n",
+    "registered": "2014-11-01T01:09:06 -04:00",
+    "latitude": -44.327952,
+    "longitude": -45.866803,
+    "tags": [
+      "exercitation",
+      "occaecat",
+      "excepteur",
+      "cillum",
+      "sunt",
+      "deserunt",
+      "deserunt"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Holman Mcdaniel"
+      },
+      {
+        "id": 1,
+        "name": "Fields Rowe"
+      },
+      {
+        "id": 2,
+        "name": "Rios Olson"
+      }
+    ],
+    "greeting": "Hello, Jeannette Best! You have 4 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "673a22a264e4404a9e360726",
+    "index": 4,
+    "guid": "8fff6413-3a28-4967-9ed6-7296ac91e313",
+    "isActive": true,
+    "balance": "$3,497.83",
+    "picture": "http://placehold.it/32x32",
+    "age": 32,
+    "eyeColor": "brown",
+    "name": "Bryan Barnett",
+    "gender": "male",
+    "company": "LUNCHPOD",
+    "email": "bryanbarnett@lunchpod.com",
+    "phone": "+1 (916) 494-3186",
+    "address": "278 Montague Terrace, Diaperville, Massachusetts, 1494",
+    "about": "Velit velit sit cillum consequat dolor ut. Et nisi anim fugiat pariatur voluptate consequat. Est amet adipisicing irure Lorem veniam minim velit incididunt esse aliquip tempor nulla amet ipsum. Id magna excepteur laborum incididunt sit magna ipsum irure eiusmod eiusmod est velit anim adipisicing. Sit mollit culpa qui fugiat irure ex magna laboris veniam incididunt enim.\r\n",
+    "registered": "2020-12-10T02:24:03 -04:00",
+    "latitude": -10.9734,
+    "longitude": 106.003951,
+    "tags": [
+      "velit",
+      "esse",
+      "duis",
+      "incididunt",
+      "officia",
+      "mollit",
+      "ea"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Melba Fry"
+      },
+      {
+        "id": 1,
+        "name": "Neva Travis"
+      },
+      {
+        "id": 2,
+        "name": "Cohen Zamora"
+      }
+    ],
+    "greeting": "Hello, Bryan Barnett! You have 6 unread messages.",
+    "favoriteFruit": "apple"
+  }
+]
